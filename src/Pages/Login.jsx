4c8f1d6fa486e107e819777e9ff8e4bab8cbaac8
Shreyas Thakare <shreyasthakare5@gmail.com>
2023-12-18 21:14:17 +0530

    18-12-23

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./login.css";
 
 import mainLogo from "../Images/mainlogo.svg"
@@ -8,20 +8,38 @@ import middleLayerSVG from "../Images/middle.svg"
 import topLayerSVG from "../Images/top.svg"
 
 const Login = () => {
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => setIsLoading(false), 3000)
+    }, [])
+
+
     return (
         <div className='loadingMain'>
             <div className="background">
-                <img className='ll' src={lowerLayerSVG} alt="lower" />
-                <img className='ml' src={middleLayerSVG} alt="lower" />
-                <img className='tl' src={topLayerSVG} alt="top" />
-
-                <img className='sun' src={sun} alt="sunsubstract" />
-
-                <img className='logo' src={mainLogo} alt="sensei logo" />
-                <p className='loadingText'>#keepasking</p>
-                <div className="progessbbarMain">
-                    <div className="progressbarSub"></div>
+                <div className="layers">
+                    <img className='ll' src={lowerLayerSVG} alt="lower" />
+                    <img className='ml' src={middleLayerSVG} alt="middle" />
+                    <img className='tl' src={topLayerSVG} alt="top" />
                 </div>
+
+                <div className="top">
+                    <div className="svgcontainers">
+
+                        <img className={`${isLoading ? "sun" : "sunzoomin"}`} src={sun} alt="sunsubstract" />
+
+                        <img className='logo' src={mainLogo} alt="sensei logo" />
+                    </div>
+                </div>
+                {isLoading && <p className="loadingText">#keepasking</p>}
+                {
+                    isLoading &&
+                    <div className="progessbarMain">
+                        <div className="progressbarSub"></div>
+                    </div>
+                }
             </div>
         </div>
     )
