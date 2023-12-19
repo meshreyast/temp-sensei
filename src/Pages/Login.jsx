c5a411887c01c37db2dcb366b2420eecb58cdef0
Shreyas Thakare <shreyasthakare5@gmail.com>
2023-12-19 16:16:50 +0530

    changes 19/12/23

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -6,10 +6,15 @@ import sun from "../Images/sun.svg"
 import lowerLayerSVG from "../Images/lower.svg"
 import middleLayerSVG from "../Images/middle.svg"
 import topLayerSVG from "../Images/top.svg"
+import indiaFlag from "../Images/emojione_flag-for-india.svg"
+import googleIcon from "../Images/icons_google.svg";
+import appleIcon from "../Images/icons_apple.svg"
 
 const Login = () => {
 
     const [isLoading, setIsLoading] = useState(true);
+
+    const [phoneNumber, setPhoneNumber] = useState();
 
     useEffect(() => {
         setTimeout(() => setIsLoading(false), 3000)
@@ -20,20 +25,48 @@ const Login = () => {
         <div className='loadingMain'>
             <div className="background">
                 <div className="layers">
-                    <img className='ll' src={lowerLayerSVG} alt="lower" />
-                    <img className='ml' src={middleLayerSVG} alt="middle" />
-                    <img className='tl' src={topLayerSVG} alt="top" />
+                    <img className={`ll ${isLoading ? "" : "layerszoomin"}`} src={lowerLayerSVG} alt="lower" />
+                    <img className={`ml ${isLoading ? "" : "layerszoomin"}`} src={middleLayerSVG} alt="middle" />
+                    <img className={`tl ${isLoading ? "" : "layerszoomin"}`} src={topLayerSVG} alt="top" />
                 </div>
 
                 <div className="top">
-                    <div className="svgcontainers">
+                    <div className={`svgcontainer ${isLoading ? "" : "h"}`}>
                         <img className={`${isLoading ? "sun" : "sunzoomin"}`} src={sun} alt="sunsubstract" />
 
                         <img className={`${isLoading ? "logo" : "logozoomin"}`} src={mainLogo} alt="sensei logo" />
                     </div>
-                    <div className="maincontent">
+                    {!isLoading && (
+                        <div className="maincontent">
+                            <p className='phonenum'>Phone number</p>
+                            <div className="pnumfield">
+                                <img src={indiaFlag} alt="Indian flag" />
+                                <p className="triangleDown"></p>
+                                <p className="vl"></p>
+                                <input type="tel"
+                                    name='phonenumber'
+                                    className='numfield'
+                                    onChange={(e) => setPhoneNumber(e.target.value)}
+                                    placeholder='Enter number' />
+                            </div>
+                            <p className="otpText">A 4 digit OTP will be sent to verify your mobile number.</p>
+                            <button className='submitBTN' type="submit">Get OTP</button>
+                            <div className="hr"></div>
+                            <div className="altLogin">
+                                <p className='altLoginText'>Or Sign in with</p>
+                                <div className="iconBox">
+                                    <div className="icons">
+                                        <img src={googleIcon} alt="google icon" />
+                                    </div>
+                                    <div className="icons">
+                                        <img src={appleIcon} alt="apple icon" />
+                                    </div>
+                                </div>
+                                <p className="altLinkText">Not registered yet?<span className='linkText'>Create an Account</span></p>
+                            </div>
+                        </div>
+                    )}
 
-                    </div>
                 </div>
                 {isLoading && <p className="loadingText">#keepasking</p>}
                 {
