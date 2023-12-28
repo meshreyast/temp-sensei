21e5f95876d8b78226306081196b4e40c5b6b83a
Shreyas Thakare <shreyasthakare5@gmail.com>
2023-12-28 22:26:14 +0530

    28-12-23 school details section

--- src/Pages/UserDashboard.jsx
+++ src/Pages/UserDashboard.jsx
@@ -28,6 +28,23 @@ const UserDashboard = () => {
                         <CounsellorCard counsellorName={item?.counsellorName} speciality={item?.speciality} rating={item?.rating} price={item?.price} />
                     ))}
                 </div>
+                <div className="schoolDetailsDiv">
+                    <h2 className='schoolDetailsText'>School Details</h2>
+                    <div className="schoolDetailsContent">
+                        <div className="keys">
+                            <p className='key'>School Name:</p>
+                            <p className='key'>Location:</p>
+                            <p className='key'>Email ID:</p>
+                            <p className='key'>Phone No.:</p>
+                        </div>
+                        <div className="values">
+                            <p className="value">Spring Dale Public School</p>
+                            <p className="value">Vadgaon Bk, Pune</p>
+                            <p className="value">abcdef@xyz.com</p>
+                            <p className="value">020-45478203</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
